fb7361af2190b31e1e644947ff1c236443c563b8
seannn9 <naeskirlu9@gmail.com>
2023-08-10 20:32:53 +0800

    added non functional buttons

--- gui.py
+++ gui.py
@@ -1,14 +1,30 @@
 from tkinter import *
-from main import convert
+from main import main
+
+button_font = ("Helveticam, 40")
+
+def convert_from(from_curr):
+    window.title("Currency Convert TO")
+    if from_curr == "USD":
+        return "USD"
+    elif from_curr == "EUR":
+        return "EUR"
+    
+def convert_to(convert_from):
+    pass
 
 window = Tk()
-window.title("Currency Convert")
+window.title("Currency Convert FROM")
 window.geometry("500x500")
 window.resizable(False, False)
 
-from_label = Label(window, text="From", font=("roboto", 20))
-from_label.pack(pady=10)
+button = Button(window, text="USD", font=button_font, width=40, command=convert_from("USD"))
+button.pack(side=TOP)
 
-button = Button(window)
+button1 = Button(window, text="PHP", font=button_font, width=40, command=convert_from("PHP"))
+button1.pack(side=TOP)
+
+button2 = Button(window, text="EUR", font=button_font, width=40, command=convert_from("EUR"))
+button2.pack(side=TOP)
 
 window.mainloop()
