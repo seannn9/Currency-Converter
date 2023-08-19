812690bd4f70fc91188b8464dd1c66856a7dce00
seannn9 <naeskirlu9@gmail.com>
2023-08-19 18:34:48 +0800

    added entry and submit for amount

--- gui.py
+++ gui.py
@@ -8,12 +8,24 @@ def destroy_all():
         widget.destroy()
 
 def create_to():
+    # create convert to options 
     button3 = Button(window, text="USD", font=button_font, width=40,command=to_USD)
     button3.pack(side=TOP)
     button4 = Button(window, text="PHP", font=button_font, width=40, command=to_PHP)
     button4.pack(side=TOP)
     button5 = Button(window, text="EUR", font=button_font, width=40, command=to_EUR)
     button5.pack(side=TOP)
+
+def create_submit():
+    def get_amount():
+        amount.get()
+    amount = Entry()
+    amount.pack()
+    submit = Button(window, text="Submit", font=("Helvetica, 20"), command=get_amount)
+    submit.pack()
+    money = get_amount()
+    return money
+
 
 def from_USD():
     destroy_all()
@@ -30,8 +42,10 @@ def from_EUR():
     create_to()
     return "EUR"
 
+# currency to convert to
 def to_USD():
     destroy_all()
+    create_submit()
     return "USD"
 
 def to_PHP():
@@ -41,7 +55,9 @@ def to_PHP():
 def to_EUR():
     destroy_all()
     return "EUR"
-    
+
+
+  
 window = Tk()
 window.title("Currency Convert")
 window.geometry("500x500")
