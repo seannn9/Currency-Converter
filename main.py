bcdff28f50da0c850730db5fb08630843a5e5668
seannn9 <naeskirlu9@gmail.com>
2023-08-19 21:46:05 +0800

    added parameter to replace input

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import requests
 
 url = 'http://data.fixer.io/api/latest?access_key=aa29df5a1c0921db9f56b133f27476dc'
 
-def main_func():
+def main_func(f, t, a):
     response = requests.get(url)
-    from_curr = input("From: ")
-    to_curr = input("To: ")
-    amount = float(input("Amount: "))
+    from_curr = f
+    to_curr = t
+    amount = float(a)
     rate = response.json()['rates'][from_curr]
     amount = amount/rate
     amount = amount * response.json()['rates'][to_curr]
     print(round(amount,2))
-    again = input("Convert again? y/n: ").lower()
-    if again == 'y':
-        main_func()
+    #again = input("Convert again? y/n: ").lower()
+    #if again == 'y':
+    #   main_func()
 
 if __name__ == '__main__':
-    main_func()
+    main_func
